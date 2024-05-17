08025ec2753b7aebb98d72c8c6a710ad44f7c163
Tejas Chendekar <tejas@Tejas-MacBook.local>
2024-05-17 04:55:42 +0100

    first commit

---
+++ src/data/tag_categories.js
@@ -0,0 +1,16 @@
+define({
+	tagCategories: {
+		"type": {
+			"description": "type of the paper",
+		},
+		"evaluation": {
+			"description": "type of evaluation used"
+		},
+		"domain": {
+			"description": "type of domain"
+		},
+		"model": {
+			"description": "type of model"
+		}
+	}
+});
